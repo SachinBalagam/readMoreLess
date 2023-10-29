225d7d003a2f5d7ed0fbd3bf8928f1b2ddaad26d
SachinBalagam <balagamsachin337@gmail.com>
2023-10-29 14:45:48 +0000

    Final Commit

--- src/components/ReadMore/index.js
+++ src/components/ReadMore/index.js
@@ -1,1 +1,43 @@
 // Write your code here
+import {useState} from 'react'
+
+import {
+  AppContainer,
+  MainContainer,
+  Heading,
+  SubHeading,
+  Image,
+  Description,
+  CustomButton,
+} from './styledComponents'
+
+const ReadMore = props => {
+  const {reactHooksDescription} = props
+
+  const [IsReadMore, SetIsReadMore] = useState(false)
+
+  const OnClickReadMore = () => {
+    SetIsReadMore(prevState => !prevState)
+  }
+  const description = IsReadMore
+    ? reactHooksDescription
+    : reactHooksDescription.slice(0, 170)
+  const ButtonText = IsReadMore ? 'Read Less' : 'Read More'
+
+  return (
+    <AppContainer>
+      <MainContainer>
+        <Heading>React Hooks</Heading>
+        <SubHeading>Hooks are a new addition to React</SubHeading>
+        <Image
+          src="https://assets.ccbp.in/frontend/hooks/react-hooks-img.png"
+          alt="react hooks"
+        />
+        <Description>{description}</Description>
+        <CustomButton onClick={OnClickReadMore}>{ButtonText}</CustomButton>
+      </MainContainer>
+    </AppContainer>
+  )
+}
+
+export default ReadMore
--- src/components/ReadMore/styledComponents.js
+++ src/components/ReadMore/styledComponents.js
@@ -1,1 +1,58 @@
 // Style your elements here
+import styled from 'styled-components'
+
+export const AppContainer = styled.div`
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+`
+
+export const MainContainer = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const Heading = styled.h1`
+  font-family: 'Roboto';
+  color: #1e293b;
+`
+
+export const SubHeading = styled.p`
+  font-family: 'Roboto';
+  color: #334155;
+`
+
+export const Image = styled.img`
+  width: 400px;
+
+  @media screen and (min-width: 768px) {
+    width: 600px;
+  }
+`
+
+export const Description = styled.p`
+  font-family: 'Roboto';
+  color: #334155;
+  width: 400px;
+
+  @media screen and (min-width: 768px) {
+    width: 600px;
+  }
+`
+
+export const CustomButton = styled.button`
+  height: 30px;
+  width: 100px;
+  background-color: #1f81ff;
+  color: #ffffff;
+  border: 0;
+  border-radius: 5px;
+  align-self: flex-start;
+  cursor: pointer;
+
+  @media screen and (min-width: 768px) {
+    align-self: center;
+  }
+`
